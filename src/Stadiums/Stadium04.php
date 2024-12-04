7e22b88762a66fc9ff4a7c5ebcf730669bda9acf
shimomo <developer@shimomo.net>
2024-12-04 14:51:14 +0900

    Fix times()

--- src/Stadiums/Stadium04.php
+++ src/Stadiums/Stadium04.php
@@ -22,29 +22,54 @@ class Stadium04 extends BaseStadium implements StadiumInterface
 
         $date = Carbon::parse($date ?? 'today')->format('Ymd');
         $baseUrl = 'https://www.heiwajima.gr.jp';
-        $crawlerFormat = '%s/asp/tbk/textvision/text/04%02dsttenji.htm';
+        $crawlerFormat = '%s/asp/kyogi/04/pc/yoso05%02d.htm';
         $crawlerUrl = sprintf($crawlerFormat, $baseUrl, $raceNumber);
         $crawler = $this->httpBrowser->request('GET', $crawlerUrl);
-        $times = $this->filterByKeys($crawler, ['td.name', '.choku_top', '.data']);
-        $chunkTimes = array_chunk(array_slice($times['.data'], 3), 6);
+        $baseXpath = 'descendant-or-self::body/div[1]/table[1]';
+        $racerNameFormat = '%s/tbody[%d]/tr[%d]/td[2]/ul/li[1]/a';
+        $timeFormat = '%s/tbody[%d]/tr[%d]/td[%d]';
 
         foreach (range(1, 6) as $bracket) {
-            $response['bracket' . $bracket . 'RacerName'] =
-                preg_split('/\d{4}/u', $this->removeSpace($times['td.name'][$bracket - 1] ?? ''))[0];
-            $response['bracket' . $bracket . 'ExhibitionTime'] =
-                (float) ($times['.choku_top'][$bracket * 2 - 2] ?? 0);
-            $response['bracket' . $bracket . 'LapTime'] =
-                (float) ($chunkTimes[$bracket - 1][0] ?? 0);
-            $response['bracket' . $bracket . 'TurnTime'] =
-                (float) ($chunkTimes[$bracket - 1][1] ?? 0);
-            $response['bracket' . $bracket . 'StraightTime'] =
-                (float) ($chunkTimes[$bracket - 1][2] ?? 0);
-            $response['bracket' . $bracket . 'LapTimeSpeed'] =
-                (float) ($chunkTimes[$bracket - 1][3] ?? 0);
-            $response['bracket' . $bracket . 'TurnTimeSpeed'] =
-                (float) ($chunkTimes[$bracket - 1][4] ?? 0);
-            $response['bracket' . $bracket . 'StraightTimeSpeed'] =
-                (float) ($chunkTimes[$bracket - 1][5] ?? 0);
+            $absenceKey = 0;
+
+            foreach (range(1, 2) as $absence) {
+                $xpath = sprintf($racerNameFormat, $baseXpath, $bracket, $absence);
+                if ($crawler->filterXPath($xpath)->count()) {
+                    $response['bracket' . $bracket . 'RacerName'] =
+                        $this->removeSpace($crawler->filterXPath($xpath)->text());
+
+                    if ($absence === 2) {
+                        $absenceKey = 1;
+                    }
+                }
+            }
+
+            foreach (range(3, 6) as $key) {
+                $xpath = sprintf($timeFormat, $baseXpath, $bracket, 1 + $absenceKey, $key);
+                if ($crawler->filterXPath($xpath)->count()) {
+                    $response[
+                        match ($key) {
+                            3 => 'bracket' . $bracket . 'LapTime',
+                            4 => 'bracket' . $bracket . 'TurnTime',
+                            5 => 'bracket' . $bracket . 'StraightTime',
+                            6 => 'bracket' . $bracket . 'ExhibitionTime',
+                        }
+                    ] = (float) $crawler->filterXPath($xpath)->text();
+                }
+            }
+
+            foreach (range(1, 3) as $key) {
+                $xpath = sprintf($timeFormat, $baseXpath, $bracket, 2 + $absenceKey, $key);
+                if ($crawler->filterXPath($xpath)->count()) {
+                    $response[
+                        match ($key) {
+                            1 => 'bracket' . $bracket . 'LapTimeSpeed',
+                            2 => 'bracket' . $bracket . 'TurnTimeSpeed',
+                            3 => 'bracket' . $bracket . 'StraightTimeSpeed',
+                        }
+                    ] = (float) $crawler->filterXPath($xpath)->text();
+                }
+            }
         }
 
         return $response;
